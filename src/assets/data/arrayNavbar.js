089f75b756fb3905d549c23e487c69fb0c99e425
LucaFaedda <lucakarl91@gmail.com>
2023-01-23 18:14:26 +0100

    scritto arrayNavbar

---
+++ src/assets/data/arrayNavbar.js
@@ -0,0 +1,208 @@
+let arrayNavbar = [
+    {
+        label: 'Home',
+        link: '#'
+    },
+    {
+        label: 'Pages',
+        link: '#',
+        children: [
+            {
+                label_2: 'Shop',
+                link_2: '#',
+                children_2: ''
+            },
+            {
+                label_2: 'Features',
+                link_2: '#',
+                children_2: [
+                    {
+                        label_3: 'About US',
+                        link_3: '#'
+                    }
+                ]
+
+            },
+            {
+                label_2: 'Events',
+                link_2: '#',
+                children_2: [
+                    {
+                        label_3: 'Event Grid',
+                        link_3: '#'
+                    },
+                    {
+                        label_3: 'Event with Sidebar',
+                        link_3: '#'
+                    },
+                    {
+                        label_3: 'Single Event',
+                        link_3: '#'
+                    }
+
+                ]
+            },
+            {
+                label_2: 'Gallery',
+                link_2: '#',
+                children_2: [
+                    {
+                        label_3: 'Gallery Grid',
+                        link_3: '#'
+                    },
+                    {
+                        label_3: 'Gallery Masonry',
+                        link_3: '#'
+                    },
+                    {
+                        label_3: 'Gallery with Sidebar',
+                        link_3: '#'
+                    }
+
+                ]
+            }
+
+        ]
+    },
+    {
+        label: 'Course Formart',
+        link: '#',
+        children: [
+            {
+                label_2: 'Default Style',
+                link_2: '#',
+                children_2: null
+            },
+            {
+                label_2: 'Classic Style',
+                link_2: '#',
+                children_2: null
+            },
+            {
+                label_2: 'Udemy Affiliate Style',
+                link_2: '#',
+                children_2: null
+            },
+            {
+                label_2: 'Offline Style',
+                link_2: '#',
+                children_2: null
+            },
+            {
+                label_2: 'Free Courses',
+                link_2: '#',
+                children_2: [
+                    {
+                        label_3: 'Default Style',
+                        link_3: '#'
+                    },
+                    {
+                        label_3: 'Classic Style',
+                        link_3: '#'
+                    },
+                    {
+                        label_3: 'Udemy Style',
+                        link_3: '#'
+                    },
+                    {
+                        label_3: 'Offline Style',
+                        link_3: '#'
+                    },
+
+                ]
+            },
+            {
+                label_2: 'Lesson Style 1',
+                link_2: '#',
+                children_2: null
+            },
+            {
+                label_2: 'Lesson Style 2',
+                link_2: '#',
+                children_2: null
+            },
+        ]
+    },
+    {
+        label: 'Courses',
+        link: '#'
+    },
+    {
+        label: 'Demos',
+        link: '#',
+        children: [
+            {
+                label_2: 'Udemy Affiliate',
+                link_2: '#',
+                children_2: null
+            },
+            {
+                label_2: 'Light LMS',
+                link_2: '#',
+                children_2: null
+            },
+            {
+                label_2: 'Dark LMS',
+                link_2: '#',
+                children_2: null
+            },
+            {
+                label_2: 'Architecture',
+                link_2: '#',
+                children_2: null
+            },
+            {
+                label_2: 'Online Academy',
+                link_2: '#',
+                children_2: null
+            },
+            {
+                label_2: 'Offline Courses',
+                link_2: '#',
+                children_2: null
+            },
+            {
+                label_2: 'Courses Hub',
+                link_2: '#',
+                children_2: null
+            },
+            {
+                label_2: 'Classsic LMS',
+                link_2: '#',
+                children_2: null
+            },
+            {
+                label_2: 'Private Instructor',
+                link_2: '#',
+                children_2: null
+            },
+            {
+                label_2: 'RTL Demo',
+                link_2: '#',
+                children_2: null
+            },
+            {
+                label_2: 'BuddyPress Demo',
+                link_2: '#',
+                children_2: null
+            },
+            {
+                label_2: 'Distance Learning',
+                link_2: '#',
+                children_2: null
+            },
+            {
+                label_2: 'Cooking Courses',
+                link_2: '#',
+                children_2: null
+            },
+
+        ]
+    },
+
+
+]
+
+export default arrayNavbar
+
+console.log(arrayNavbar)
