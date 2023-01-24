40b52b3cbd2e83378b968ed475a0907d4088b2fe
LucaFaedda <lucakarl91@gmail.com>
2023-01-24 15:48:42 +0100

    cambiato appIcone e appCategory

--- src/assets/data/arrayCourses.js
+++ src/assets/data/arrayCourses.js
@@ -25,6 +25,94 @@ let arrayCourses = [
                 icons_2: 'fa-solid fa-sack-dollar',
             },
         ]
-    }
+    },
+    {
+        label: 'Design',
+        icons: 'fa-solid fa-palette',
+        stato: false,
+        chidlren: [
+            {
+                label_2: '3D & animation',
+                icons_2: 'fa-solid fa-vr-cardboard',
+            },
+            {
+                label_2: 'Design Tools',
+                icons_2: 'fa-solid fa-laptop',
+            },
+            {
+                label_2: 'Game Design',
+                icons_2: 'fa-solid fa-gamepad',
+            },
+            {
+                label_2: 'User Experience',
+                icons_2: 'fa-solid fa-house',
+            },
+            {
+                label_2: 'Web Design',
+                icons_2: 'fa-solid fa-desktop',
+            },
+        ]
+    },
+    {
+        label: 'Development',
+        icons: 'fa-solid fa-gear',
+        stato: false,
+        chidlren: [
+            {
+                label_2: 'Data Science',
+                icons_2: 'fa-solid fa-arrow-right',
+            },
+            {
+                label_2: 'Game development',
+                icons_2: 'fa-solid fa-gamepad',
+            },
+            {
+                label_2: 'Mobile Apps',
+                icons_2: 'fa-solid fa-mobile-screen-button',
+            },
+            {
+                label_2: 'Programming Languages',
+                icons_2: 'fa-regular fa-file-code',
+            },
+            {
+                label_2: 'Software Engineering',
+                icons_2: 'fa-solid fa-code',
+            },
+            {
+                label_2: 'Web Development',
+                icons_2: 'fa-solid fa-arrow-right',
+            },
+        ]
+    },
+    {
+        label: 'Lifestyle',
+        icons: 'fa-regular fa-face-smile',
+        stato: false,
+        chidlren: [
+            {
+                label_2: 'Arts & Craftys',
+                icons_2: 'fa-solid fa-paintbrush',
+            },
+            {
+                label_2: 'Food & Beverage',
+                icons_2: 'fa-solid fa-bell-concierge',
+            },
+        ]
+    },
+    {
+        label: 'Office Productivity',
+        icons: 'fa-regular fa-wallet',
+        stato: false,
+        chidlren: [
+            {
+                label_2: 'Google',
+                icons_2: 'fa-solid fa-users',
+            },
+        ]
+    },
+
 
 ]
+
+export default arrayCourses
+console.log(arrayCourses)
