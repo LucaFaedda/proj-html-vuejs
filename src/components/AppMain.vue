0acaf1ad3313d7558e7512457f0c9924d8a5d53c
LucaFaedda <lucakarl91@gmail.com>
2023-01-25 13:19:48 +0100

    Popular course card done

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -21,54 +21,136 @@ export default {
             <!-- <img src="../assets/img/slide-1.jpg"> -->
         </div>
         <div class="container-fluid padding-container my-5">
+            <div class="row">
+                <div class="col-12 mb-4">
+                    <h3>Popular Development Courses</h3>
+                </div>
+            </div>
             <div class="row ">
-                <div class="col-12 d-flex justify-content-between ">
+                <div class="col-12 d-flex justify-content-between card-popular ">
                     <div class="card" style="width: 17rem;">
-                    <img src="../assets/img/895786_7b4b_2-272x161.jpg" class="card-img-top" alt="...">
+                    <img src="../assets/img/895786_7b4b_2-272x161.jpg" class="card-img-top" alt="apple-products">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                    </div>
-                    <div class="card" style="width: 17rem;">
-                    <img src="../assets/img/246154_d8b0_3-544x322.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                    </div>
-                    <div class="card" style="width: 17rem;">
-                    <img src="../assets/img/752950_b773-272x161.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <p class="card-title">Development</p>
+                        <p class="card-text">The complete iOS & Swift 3 Developer Course</p>
+                        <hr>
+                        <div class="d-flex justify-content-between">
+                            <div>
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/staremptyl.svg">
+                            </div>
+                            <div>
+                                <h6 >Free</h6>
+                            </div>
+                        </div>
                     </div>
                     </div>
                     <div class="card" style="width: 17rem;">
-                    <img src="../assets/img/1253188_58f7_2-272x161.jpg" class="card-img-top" alt="...">
+                    <img src="../assets/img/246154_d8b0_3-544x322.jpg" class="card-img-top" alt="HTML-CSS">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <p class="card-title">Development</p>
+                        <p class="card-text">Web Design for Beginners: Real World Coding in HTML & CSS</p>
+                        <hr>
+                        <div class="d-flex justify-content-between">
+                            <div>
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/staremptyl.svg">
+                            </div>
+                            <div>
+                                <p class="m-0 old-price">$129.99</p>
+                                <h6 >$65</h6>
+                            </div>
+                        </div>
                     </div>
                     </div>
                     <div class="card" style="width: 17rem;">
-                    <img src="../assets/img/895786_7b4b_2-272x161.jpg" class="card-img-top" alt="...">
+                    <img src="../assets/img/752950_b773-272x161.jpg" class="card-img-top" alt="Git">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <p class="card-title">Development</p>
+                        <p class="card-text">GitHub Ultimate: MAster Git and GitHub - Beginner to Expert</p>
+                        <hr>
+                        <div class="d-flex justify-content-between">
+                            <div>
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/staremptyl.svg">
+                            </div>
+                            <div>
+                                <p class="m-0 old-price">$99.99</p>
+                                <h6 >$50</h6>
+                            </div>
+                        </div>
                     </div>
                     </div>
                     <div class="card" style="width: 17rem;">
-                    <img src="../assets/img/895786_7b4b_2-272x161.jpg" class="card-img-top" alt="...">
+                    <img src="../assets/img/1253188_58f7_2-272x161.jpg" class="card-img-top" alt="apple-products-2">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <p class="card-title">Development</p>
+                        <p class="card-text">The Complete iOS 11 & Swift Developer Course - Build 20 Apps</p>
+                        <hr>
+                        <div class="d-flex justify-content-between">
+                            <div>
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/staremptyl.svg">
+                            </div>
+                            <div>
+                                <p class="m-0 old-price">$199.99</p>
+                                <h6 >$100</h6>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="card" style="width: 17rem;">
+                    <img src="../assets/img/919872_ed54_6-272x161.jpg" class="card-img-top" alt="android">
+                    <div class="card-body">
+                        <p class="card-title">Development</p>
+                        <p class="card-text">Android Java Masterclass - Become an App Developer</p>
+                        <hr>
+                        <div class="d-flex justify-content-between">
+                            <div>
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/staremptyl.svg">
+                            </div>
+                            <div>
+                                <p class="m-0 old-price">$99.99</p>
+                                <h6 >$50</h6>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="card" style="width: 17rem;">
+                    <img src="../assets/img/951684_9c1a_2-272x161.jpg" class="card-img-top" alt="xamarin">
+                    <div class="card-body">
+                        <p class="card-title">Development</p>
+                        <p class="card-text">Xamarin Forms: Build Native Cross-platform Apps with C#</p>
+                        <hr>
+                        <div class="d-flex justify-content-between">
+                            <div>
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/starfull.svg">
+                                <img src="../assets/img/staremptyl.svg">
+                            </div>
+                            <div>
+                                <p class="m-0 old-price">$189.99</p>
+                                <h6 >$95</h6>
+                            </div>
+                        </div>
                     </div>
                     </div>
                 </div>
@@ -88,20 +170,31 @@ export default {
         background-image: url('../assets/img/slide-1.jpg'),  linear-gradient(63deg, rgba(255,68,80,1) 51%, rgba(249,249,249,1) 51%, rgba(241,241,241,1) 51%);
         background-repeat: no-repeat;
         background-position: right;
-
-         
-
             .container{
                 padding: 100px 0px;
                 color:#fff;
-                    h2{
-                        font-size: 60px;
-                        
-                    }
+                h2{
+                    font-size: 60px;       
+                }
             
          }
-        
-        
+    }
+    .card{
+        border-radius: 0%;
+        margin: 0px 5px;
+        img{
+            border-radius: 0%;
+        }
+        .card-title{
+            color: $colorGrey;
+            font-size: 14px;
+        }
 
+        .old-price{
+            color:  $colorGrey;
+            text-decoration: line-through;
+            font-size: 14px;
+           
+        }
     }
 </style>
